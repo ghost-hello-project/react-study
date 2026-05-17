d437264e3b965846ad006fd6f8e214581fca83a1
laolang2016 <xiaodaima2016@163.com>
2026-05-17 14:41:53 +0800

    code(react-base-setup): 引入 antd

--- code/react-base-setup/src/pages/home/home.tsx
+++ code/react-base-setup/src/pages/home/home.tsx
@@ -1,5 +1,17 @@
-const Home = () => {
-    return <h1>首页</h1>
+import type React from 'react'
+import { Flex } from 'antd'
+import { useNavigate } from 'react-router'
+import { Button } from 'antd'
+
+const Home: React.FC = () => {
+    const navigate = useNavigate()
+    return (
+        <>
+            <Flex gap="medium">
+                <Button onClick={() => navigate('/about')}>关于</Button>
+            </Flex>
+        </>
+    )
 }
 
 export default Home
